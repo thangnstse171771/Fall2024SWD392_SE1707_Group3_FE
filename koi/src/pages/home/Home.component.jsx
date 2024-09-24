7ffa2514ea68389a427ae4ed163fcaf824cb3e6c
QuyDuong162 <duonghqse171776@fpt.edu.vn>
2024-09-24 13:48:54 +0700

    update home page

--- koi/src/pages/home/Home.component.jsx
+++ koi/src/pages/home/Home.component.jsx
@@ -1,11 +1,22 @@
-import About from "../About/About";
+import KoiFishArt from "../../assets/Koi-Fish-yinyang-01_grande.webp";
+import "./Home.scss";
 
 function Home() {
   return (
-    <h1>
-      Home Page 1
-      <About />
-    </h1>
+    <div className="home-page">
+      <div className="home-card">
+        <img src={KoiFishArt} alt="Fish art" className="fish-art"/>
+        <div className="welcome-text">
+          <h1>Welcome to Koi care system</h1>
+          <p className="welcome-context">Koi care system is an online Koi fish health tracking system designed to 
+            help you take care of your Koi fishes at home.</p>
+        </div>
+      </div>
+
+      <div>
+        
+      </div>
+    </div>
   );
 }
 
