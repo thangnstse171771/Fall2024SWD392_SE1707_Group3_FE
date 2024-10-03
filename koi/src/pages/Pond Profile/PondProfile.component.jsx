a1de9f97d4ba8b7315550b783cf3abbda658884b
QuyDuong162 <duonghqse171776@fpt.edu.vn>
2024-10-03 16:33:00 +0700

    update pond profile UI

--- koi/src/pages/Pond Profile/PondProfile.component.jsx
+++ koi/src/pages/Pond Profile/PondProfile.component.jsx
@@ -1,10 +1,25 @@
-import { useState, useEffect } from "react";
-import "./PondProfile.scss"
+import "./PondProfile.scss";
+import PondProfileInfo from "./PondProfileInfo.component";
+import { Divider } from "antd";
 
 function PondProfile() {
   return (
     <div className="pond-profile-page">
-      Pond Profile
+      <div className="pond-profile-header">
+        <h1>Pond profile</h1>
+      </div>
+      <div className="pond-profile-body">
+        <Divider style={{ borderColor: "#7cb305" }}>Pond Info</Divider>
+        <div className="pond-profile-info">
+          <PondProfileInfo />
+        </div>
+        <Divider style={{ borderColor: "#7cb305" }}>Fish List</Divider>
+        <div className="pond-profile-fish-list">Fish list</div>
+        <Divider style={{ borderColor: "#7cb305" }}>Water Parameter</Divider>
+        <div className="pond-profile-water-parameter">Pararmeter</div>
+      </div>
     </div>
   );
 }
+
+export default PondProfile;
