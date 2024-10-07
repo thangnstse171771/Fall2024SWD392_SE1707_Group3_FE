49da95faf0516b5c926023fc4df74cd7eb5527e7
QuyDuong162 <duonghqse171776@fpt.edu.vn>
2024-10-07 17:31:42 +0700

    commit to pull code

---
+++ koi/src/pages/My Pond/ConfirmDeletePond.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Modal } from "antd";
+
+const ConfirmDeletePond = () => {
+  return (
+    <Modal>
+      <div>ConfirmDeletePond.component</div>
+    </Modal>
+  );
+};
+
+export default ConfirmDeletePond.component;
