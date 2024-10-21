d7104e96667abc2cb8b207bd9a3e532ea91421c8
TruongNguyen07122003 <96875408+TruongNguyen07122003@users.noreply.github.com>
2024-10-21 17:22:49 +0700

    fix ui: delete and role acess for delete function onlu for admin

--- koi/src/pages/ManageAccount/DeleteUser.jsx
+++ koi/src/pages/ManageAccount/DeleteUser.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { Button } from "antd";
 
 export default function DeleteUser({ customerId, onDelete }) {
+  const userType = localStorage.getItem("usertype");
   return (
-    <Button
-      style={{ color: "rgb(180,0,0)" }}
-      onClick={() => onDelete(customerId)} // Gọi hàm xóa với customerId
-    >
-      Delete
-    </Button>
+    <>
+      {userType === "Admin" && (
+        <Button
+          style={{ color: "rgb(180,0,0)" }}
+          onClick={() => onDelete(customerId)} // Gọi hàm xóa với customerId
+        >
+          Delete
+        </Button>
+      )}
+    </>
   );
 }
