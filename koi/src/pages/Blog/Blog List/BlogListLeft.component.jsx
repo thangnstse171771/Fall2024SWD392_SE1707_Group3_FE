32087a8c9efd90553fecf39ed4fe4ed5505ad789
QuyDuong162 <duonghqse171776@fpt.edu.vn>
2024-10-24 16:13:27 +0700

    render recommended products UI, call api for view blog list

--- koi/src/pages/Blog/Blog List/BlogListLeft.component.jsx
+++ koi/src/pages/Blog/Blog List/BlogListLeft.component.jsx
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import api from "../../../config/axios";
 
 const BlogListLeft = () => {
+  const [featuredBlog, setFeaturedBlog] = useState({});
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchFeaturedBlog = async () => {
+    try {
+      const response = await api.get(`/api/blog/getBlogById/5`);
+      setFeaturedBlog(response.data);
+    } catch (error) {
+      toast.error("Error fetching featured blog!");
+      console.error("Error fetching featured blog:", error);
+    }
+  };
+
+  const fetchAllBlogs = async () => {
+    try {
+      const response = await api.get(`/api/blog/getAllBlogs`);
+      setBlogs(response.data);
+    } catch (error) {
+      toast.error("Error fetching blogs!");
+      console.error("Error fetching featured blog:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFeaturedBlog();
+    fetchAllBlogs();
+  }, []);
+
   return (
     <div>
       <div className="featured-card">
@@ -11,24 +41,28 @@ const BlogListLeft = () => {
             alt="Featured"
           />
           <div className="featured-overlay">
-            <h4 className="featured-title">Bridge of Progress</h4>
-            <h4 className="featured-desc">Description</h4>
+            <h4 className="featured-title">{featuredBlog.blogTitle}</h4>
+            <h4 className="featured-desc">{featuredBlog.blogContent}</h4>
           </div>
         </div>
       </div>
 
       <div className="small-blog-list">
-        <div className="small-blog-card">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc-oBPjDYorVZTRXD0AcVk-fLx6t2BR_loNQ&s"
-            alt="blog image"
-            className="small-blog-image"
-          />
-          <div className="small-blog-content">
-            <h4 className="small-blog-title">Title</h4>
-            <h5 className="small-blog-description">Description</h5>
+        {blogs.slice(0, 3).map((blog) => (
+          <div key={blog.blogId} className="small-blog-card">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc-oBPjDYorVZTRXD0AcVk-fLx6t2BR_loNQ&s"
+              alt="blog image"
+              className="small-blog-image"
+            />
+            <div className="small-blog-content">
+              <h4 className="small-blog-title">{blog.blogTitle}</h4>
+              <h5 className="small-blog-description">
+                {blog.blogContent.slice(0, 50)}...
+              </h5>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
